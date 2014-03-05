b41d7bc582db83cff8cc24d026cefe6f67d21889
Elliot Smith <elliot.smith@intel.com>
2014-03-05 21:37:01 +0000

    Bump versions manually

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
         commit: true,
         push: true,
 
-        bump: true,
+        bump: false,
         tag: true,
         pushTags: true,
         npm: true,
